e816ce11f808b4da7b4d0ad32d88dfb399a293cb
guswns1659 <guswns1659@gmail.com>
2021-08-08 13:00:43 +0900

    feat: Create Account domain entity

--- src/main/java/com/titanic/buckpal/account/domain/Account.java
+++ src/main/java/com/titanic/buckpal/account/domain/Account.java
@@ -1,5 +1,25 @@
 package com.titanic.buckpal.account.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Value;
+
+/**
+ * An account that hold a certain amount of money. An Account object only contains a window of the latest account
+ * activities. The total balance of the account is the sum of a baseline balance that was valid before the first
+ * activity in the window and the sum of the activity values.
+ */
+@AllArgsConstructor(access = AccessLevel.PRIVATE) // always private
 public class Account {
+
+    @Getter
+    private final AccountId id;
+
+    @Value
+    public static class AccountId {
+        Long value;
+    }
+
 
 }
